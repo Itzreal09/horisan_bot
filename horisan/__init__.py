26a379b5a072e822b944732746067a6bcb663f8d
Itzreal09 <101443633+Itzreal09@users.noreply.github.com>
2022-05-11 14:49:53 +0530

    Update __init__.py

--- horisan/__init__.py
+++ horisan/__init__.py
@@ -216,7 +216,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from Yumeko.modules.helper_funcs.handlers import (
+from horisan.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
